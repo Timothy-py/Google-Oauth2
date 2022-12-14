7466481f561cfe220ac68ca81595ebca28883bc8
Timothy-py <adeyeyetimothy33@gmail.com>
2022-12-14 15:16:04 +0100

    implemented google oauth utility

---
+++ WithoutPassport/utils/google-client.js
@@ -0,0 +1,10 @@
+const googleAPI = require('googleapis')
+
+const oauth2Client = new googleAPI.Auth.OAuth2Client({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: 'http://localhost:3000/api/google'
+})
+
+
+module.exports = oauth2Client
