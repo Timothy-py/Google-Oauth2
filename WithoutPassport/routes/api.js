1f71bb33c834b612e873db0dd3e1d66b61386c0d
Timothy-py <adeyeyetimothy33@gmail.com>
2022-12-14 23:49:20 +0100

    implemented oauth2 screen

--- WithoutPassport/routes/api.js
+++ WithoutPassport/routes/api.js
@@ -1,7 +1,34 @@
 const router = require('express').Router();
+const url_parser = require('url')
+const axios = require('axios')
+
+const googleClient = require('../utils/google-client')
 
 router.get('/index', (req, res)=>{
     return res.status(200).send('Authentication Service with Google Oauth2 without Passportjs')
 })
 
+router.get('/google', (req, res)=>{
+    const scopes = [
+        'https://www.googleapis.com/auth/userinfo.profile',
+        'https://www.googleapis.com/auth/userinfo.email'
+    ]
+
+    const generateUrl = googleClient.generateAuthUrl({
+        access_type: 'offline',
+        prompt: 'consent',
+        scope: scopes
+    })
+    res.redirect(generateUrl)
+})
+
+router.get('/google/callback', async (req, res)=>{
+    const url = req.url
+    const parse = url_parser.parse(url, true)
+    const code = parse.query.code
+    
+    // const {tokens} = 
+})
+
 module.exports = router;
+
--- WithoutPassport/utils/google-client.js
+++ WithoutPassport/utils/google-client.js
@@ -3,7 +3,7 @@ const googleAPI = require('googleapis')
 const oauth2Client = new googleAPI.Auth.OAuth2Client({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: 'http://localhost:3000/api/google'
+    redirectUri: 'http://localhost:3000/api/google/callback'
 })
 
 
