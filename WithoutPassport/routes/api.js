4a52284426c7589514c658c872acb8d18432366f
Timothy-py <adeyeyetimothy33@gmail.com>
2022-12-14 13:40:06 +0100

    setup express project

---
+++ WithoutPassport/routes/api.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+
+router.get('/index', (req, res)=>{
+    return res.status(200).send('Authentication Service with Google Oauth2 without Passportjs')
+})
+
+
+
+module.exports = router;
---
+++ WithoutPassport/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config()
+const express = require('express')
+
+const routes = require('./routes/api')
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/api', routes)
+
+
+app.listen('3000', ()=>{
+    console.log('App is listening on PORT:3000')
+})
+
+module.exports = app;
